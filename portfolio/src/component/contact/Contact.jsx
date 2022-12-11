17c024584bad8555ba3a1a1b9ad324dacb3c7a39
Arham Khan <81346957+Arham47@users.noreply.github.com>
2022-12-11 20:16:19 +0500

    completed

--- portfolio/src/component/contact/Contact.jsx
+++ portfolio/src/component/contact/Contact.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import  { useRef } from 'react';
+import emailjs from "emailjs-com"
+import {MdOutlineEmail} from "react-icons/md"
+import {RiMessengerLine} from "react-icons/ri"
+import {BsWhatsapp} from "react-icons/bs"
+import "./contact.css"
 
 function Contact() {
+  const form =useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8e8ho2d', 'template_l9vbfn7', form.current, 'v3ZQvTGKkmHSgM99n')
+
+      .then((result) => {
+          console.log(result.text);
+          
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact ME</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className="contact_option_icon"/>
+            <h4>E-mail</h4>
+            <h5>i.am.arhamkhan25@gmail.com</h5>
+            <a href='mailto:i.am.arhamkhan25@gmail.com' target="_blank">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <RiMessengerLine className="contact_option_icon"/>
+            <h4>Messanger</h4>
+            <h5>Muhammad Arham Khan</h5>
+            <a href='https://m.me/fraudibanda' target="_blank">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp className="contact_option_icon"/>
+            <h4>WhatsApp</h4>
+            <h5>+92 335 990 8281</h5>
+            <a href=' https://wa.me/+923359908281' target="_blank">Send a message</a>
+          </article>
+
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name"placeholder='Your Full Name' required/>
+          <input type="email" name="email"placeholder='i.am.arhamkhan25@gmail.com' required/>
+          <textarea name="message" id="" rows="7" required placeholder='Your Message'></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+
+        </form>
+      </div>
+    </section>
   )
 }
 
--- portfolio/src/component/footer/Footer.jsx
+++ portfolio/src/component/footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {FaFacebookF} from "react-icons/fa"
+import {FiInstagram} from "react-icons/fi"
+import "./footer.css"
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_logo'> Muhammad Arham</a>
+      <ul className='permalink'>
+        <li><a href="#">HOME</a></li> 
+        <li><a href="#about">ABOUT</a></li> 
+        <li><a href="#experience">EXPERIENCE</a></li> 
+        <li><a href="#portfolio">PORTFOLIO</a></li> 
+        <li><a href="#testimonial">TESTIMONIAL</a></li> 
+        <li><a href="#contact">CONTACT</a></li> 
+      </ul>
+      <div className="footer_socials">
+      <a href="https://facebook.com/fraudibanda" target="_blank"><FaFacebookF/></a>
+      <a href="https://instagram.com/i.am.arhamkhan" target="_blank"><FiInstagram/></a>
+
+      </div>
+    <div className="footer_copyright">
+      <small>&copy; Muhammad Arham Khan Portfolio .... All rights are reserved</small>
+    </div>
+    </footer>
   )
 }
 
